397d5a662653117e0496b6cd2bf479c5177d85d0
Tilak Mehrotra <42639029+TilakMehrotra@users.noreply.github.com>
2025-08-16 05:28:00 +0530

    push

---
+++ step1_basics/02.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //int, float, long, long double,
+    //string and getline
+    
+    // string str;
+    // cin >> str;
+    // cout << str;
+
+    // string str;
+    // getline(cin,str)
+    // cout << str;
+
+    return 0;
+}
